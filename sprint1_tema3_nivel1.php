7138d32898f10c1177089f4e0d734ef88c5fe994
CristinaSoy <ccardonaaltes@hotmail.com>
2024-12-10 17:30:35 +0100

    ex1.bucle foreach per mostrar elements i bug solucionat a ex2

--- sprint1_tema3_nivel1.php
+++ sprint1_tema3_nivel1.php
@@ -10,21 +10,12 @@ $nombres[]=2;
 $nombres[]=3;
 $nombres[]=4;
 
-echo $nombres[0]. "<br>";
-echo $nombres[1]. "<br>";
-echo $nombres[2]. "<br>";
-echo $nombres[3]. "<br>";
-echo $nombres[4]. "<br>";
 
-/*
-for ($i=0; $i<count($nombres);$i++) {
-    echo $nombres[$i] . "<br>";
-}
 
 foreach($nombres as $key => $value) {
     echo "$value <br>";
 }
-*/
+
 echo "Exercici 2 <br>";
 
 $X = array (10, 20, 30, 40, 50,60);
@@ -35,14 +26,15 @@ echo count($X)."<br>";
 
 echo "Exercici 3. True si totes les paraules contenen el caracter <br>";
 
-$paraules = array ("dissabte", "tempesta", "peseta", "cavall", "moneda");
-$caracter = "e";
+$paraules = array ("dissabte", "espanta", "peseta", "cavall", "moneda");
+$caracter = "E";
 
 echo trobaCaracter($paraules, $caracter)? "true" : "false";
 
 function trobaCaracter($paraules, $caracter) : bool{
     foreach ($paraules as $paraula) {
-        if (strpos($paraula, $caracter) == false) {
+        if (stripos($paraula, $caracter) === false) {
+            echo "La paraula $paraula no conté el caracter $caracter. <br>"; 
             return false;
         } 
     }
